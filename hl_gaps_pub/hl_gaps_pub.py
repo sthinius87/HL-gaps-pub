5ef2d530b81f682c7c2bfc369c29a47599494bea
Sascha Thinius <sascha.thinius@ifam.fraunhofer.de>
2025-03-26 08:56:09 +0100

    return fixes

--- hl_gaps_pub/hl_gaps_pub.py
+++ hl_gaps_pub/hl_gaps_pub.py
@@ -96,15 +96,11 @@ def parse_sdf_db(filepath: str) -> pd.DataFrame:
     --------
     >>> # Create a dummy SDF file for the example
     >>> with open("temp.sdf", "w", encoding="utf-8") as f: # Added encoding
-    ...     num_written1 = f.write("CDK     145124856\n\n> <SMILES>\nCC\n\n$$$$\n")
-    ...     num_written2 = f.write("CDK     158924962\n\n> <SMILES>\nCCC\n\n$$$$\n")
-    >>> print(num_written1)
-    39
-    >>> print(num_written2)
-    40
+    ...     _ = f.write("CDK     145124856\n\n> <SMILES>\nCC\n\n$$$$\n")
+    ...     _ = f.write("CDK     158924962\n\n> <SMILES>\nCCC\n\n$$$$\n")
     >>> df = parse_sdf_db("temp.sdf")
     >>> print(df)
-                 2dsdf SMILES
+                     2dsdf SMILES
     0  [CDK     145124856]     CC
     1  [CDK     158924962]    CCC
 
@@ -333,7 +329,7 @@ def calculate_gap(molecule: Chem.Mol, method: str, accuracy: float, temperature:
     >>> mol = Chem.MolFromSmiles("CC")  # Ethane
     >>> mol_with_hs = Chem.AddHs(mol)
     >>> params = AllChem.ETKDGv3()
-    >>> Chem.AllChem.EmbedMultipleConfs(mol_with_hs, numConfs=2, params=params)
+    >>> _ = Chem.AllChem.EmbedMultipleConfs(mol_with_hs, numConfs=2, params=params)
     >>> gap = calculate_gap(mol_with_hs, "GFN2-xTB", 1.0, 300.0)
     >>> isinstance(gap, float)
     True
